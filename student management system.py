0e85b4a6428865c83dac1fc2402b745abe5aaf10
vinuthaag07-pixel <vinuthaag07@gmail.com>
2026-09-20 11:09:38 +0530

    Add search and delete functionality to student system

--- student management system.py
+++ student management system.py
@@ -4,7 +4,9 @@ while True:
     print("\n===== Student Management System =====")
     print("1. Add Student")
     print("2. View Students")
-    print("3. Exit")
+    print("3. Search Student")
+    print("4. Delete Student")
+    print("5. Exit")
 
     choice = int(input("Enter your choice: "))
 
@@ -35,6 +37,35 @@ while True:
                 print("--------------------")
 
     elif choice == 3:
+        search_name = input("Enter student name to search: ")
+        found = False
+
+        for student in students:
+            if student["name"].lower() == search_name.lower():
+                print("Student Found!")
+                print("Name:", student["name"])
+                print("Age:", student["age"])
+                print("Course:", student["course"])
+                found = True
+
+        if not found:
+            print("Student not found.")
+
+    elif choice == 4:
+        delete_name = input("Enter student name to delete: ")
+        found = False
+
+        for student in students:
+            if student["name"].lower() == delete_name.lower():
+                students.remove(student)
+                print("Student deleted successfully!")
+                found = True
+                break
+
+        if not found:
+            print("Student not found.")
+
+    elif choice == 5:
         print("Thank you for using Student Management System!")
         break
 
